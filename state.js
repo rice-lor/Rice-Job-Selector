a07396403db102b7e9eba12de632aff2d11e3d79
rice-lor <nguyendinhphuhao@gmail.com>
2025-08-29 20:15:15 +0700

    Delete state.js

--- state.js
+++
@@ -1,6 +0,0 @@
-const NUI_EXTRA_DELAY = 10; // Minimal delay for NUI interactions
-
-export default {
-    NUI_EXTRA_DELAY,
-    // Add other minimal state properties if absolutely necessary for job_selection_menu.js
-};
